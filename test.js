1fdf21d9d5442b279c9cd291240eea133863d84d
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-23 18:47:26 -0300

    Sixth video

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
+// the first alert
 alert("Hello, World!");
+
+// the second alert
+alert("Hello, World!");
+
+// make th image invisible
+// make the image from top to bottom
+// make the image visible

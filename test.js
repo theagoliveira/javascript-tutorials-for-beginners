615697b7a6c2319b2f45c53729ffe13c4cd3e73a
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-27 19:56:08 -0300

    Twenty-second video

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
-var average = 7;
+var a = 7;
+var b = 5;
 
-function getAverage(a, b, c, d, e, f) {
-  var average = (a + b + c + d + e + f) / 6; // local variable
-  console.log(average);
-  return average;
-}
+console.log(a + b);
 
-var myResult = getAverage(7, 8, 9, 10, 11, 12); // global variable
-
-function logResult() {
-  console.log("the average is " + myResult);
-}
-logResult();
+console.log(Math.PI);

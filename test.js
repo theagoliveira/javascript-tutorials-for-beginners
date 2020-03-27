7d547e1371053715f1d63f0a94eed0127fc9ee0f
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-27 19:21:53 -0300

    Twentieth video

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-var links = document.getElementsByTagName("a");
-
-for (i = 0; i < links.length; i++) {
-  links[i].className = "link" + i;
+function getAverage(a, b, c, d, e, f) {
+  var average = (a + b + c + d + e + f) / 6;
+  console.log(average);
+  return average;
 }
+
+var myResult = getAverage(7, 8, 9, 10, 11, 12);
+console.log("the average is " + myResult);

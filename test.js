da0c7c23d019153809bd3b090361efdba7a0ce83
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-27 23:35:10 -0300

    Thirty-first video

--- test.js
+++ test.js
@@ -1,18 +1,26 @@
-var Car = function(maxSpeed, driver) {
-  this.maxSpeed = maxSpeed;
-  this.driver = driver;
-  this.drive = function(speed, time) {
-    console.log(speed * time);
-  };
-  this.logDriver = function() {
-    console.log("driver name is " + this.driver);
-  };
-};
+var birthday = new Date(1985, 0, 15, 11, 15, 25);
+var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
 
-var myCar = new Car(70, "Ninja Man 1");
-var myCar2 = new Car(80, "Ninja Man 2");
-var myCar3 = new Car(90, "Ninja Man 3");
-var myCar4 = new Car(100, "Ninja Man 4");
+// get the month of the date (0 - 11)
+console.log(birthday.getMonth());
 
-myCar.drive(40, 5);
-myCar3.logDriver();
+// get the full year (YYYY)
+console.log(birthday.getFullYear());
+
+// get the date of the month (1  - 31)
+console.log(birthday.getDate());
+
+// get the dat of the week (0 - 6)
+console.log(birthday.getDay());
+
+// get the hour of the date (0 - 23)
+console.log(birthday.getHours());
+
+// get the number of milliseconds since 1st Jan 1970
+console.log(birthday.getTime());
+
+if (birthday.getTime() == birthday2.getTime()) {
+  console.log("birthdays are equal");
+} else {
+  console.log("birthdays are not equal");
+}

7839dfdd84f464a0b85061a878765e78e3aec7ad
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-27 22:12:35 -0300

    Twenty-ninth video

--- test.js
+++ test.js
@@ -1,23 +1,16 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Thiago";
-myCar.drive = function() {
-  console.log("now driving");
-};
-
-myCar.drive();
+console.log(this);
 
 var myCar2 = {
   maxSpeed: 70,
   driver: "Jack",
   drive: function(speed, time) {
     console.log(speed * time);
+  },
+  logDriver: function() {
+    console.log("driver name is " + this.driver);
   }
 };
 
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);

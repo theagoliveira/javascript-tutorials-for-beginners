357b36155a83217e019830eb68460839f27b5bb5
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-30 03:40:26 -0300

    Forty-first video

--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents() {
+  var content = document.getElementById("content");
+  var button = document.getElementById("show-more");
 
-button.onclick = function() {
-  if (content.className == "open") {
-    // Shrink the box
-    content.className = "";
-    button.innerHTML = "Show More";
-  } else {
-    // Expand the box
-    content.className = "open";
-    button.innerHTML = "Show Less";
-  }
+  button.onclick = function() {
+    if (content.className == "open") {
+      // Shrink the box
+      content.className = "";
+      button.innerHTML = "Show More";
+    } else {
+      // Expand the box
+      content.className = "open";
+      button.innerHTML = "Show Less";
+    }
+  };
+}
+
+window.onload = function() {
+  setUpEvents();
 };

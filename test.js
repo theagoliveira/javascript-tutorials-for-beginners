5af8d308f27f729327bdb86e17818299417f5ab7
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-30 04:33:21 -0300

    Forty-fourth video

---
+++ test.js
@@ -0,0 +1,12 @@
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
+
+myForm.onsubmit = function() {
+  if (myForm.name.value == "") {
+    message.innerHTML = "Please enter a name.";
+    return false;
+  } else {
+    message.innerHTML = "";
+    return true;
+  }
+};

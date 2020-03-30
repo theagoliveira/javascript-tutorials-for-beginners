70ed7c8ef9ce49a102fd891f8e0ae71225369f9a
Thiago Cavalcante <thiago.kun@gmail.com>
2020-03-30 03:27:55 -0300

    Fortieth video

--- test.js
+++ test.js
@@ -1,26 +1,14 @@
-var birthday = new Date(1985, 0, 15, 11, 15, 25);
-var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
-// get the month of the date (0 - 11)
-console.log(birthday.getMonth());
-
-// get the full year (YYYY)
-console.log(birthday.getFullYear());
-
-// get the date of the month (1  - 31)
-console.log(birthday.getDate());
-
-// get the dat of the week (0 - 6)
-console.log(birthday.getDay());
-
-// get the hour of the date (0 - 23)
-console.log(birthday.getHours());
-
-// get the number of milliseconds since 1st Jan 1970
-console.log(birthday.getTime());
-
-if (birthday.getTime() == birthday2.getTime()) {
-  console.log("birthdays are equal");
-} else {
-  console.log("birthdays are not equal");
-}
+button.onclick = function() {
+  if (content.className == "open") {
+    // Shrink the box
+    content.className = "";
+    button.innerHTML = "Show More";
+  } else {
+    // Expand the box
+    content.className = "open";
+    button.innerHTML = "Show Less";
+  }
+};
